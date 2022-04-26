af2a7362d274d9f0cfe35eb066beed43942a8f86
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-04-26 01:20:15 +0100

    fresh start

--- assets/assets/js/script.js
+++ assets/assets/js/script.js
@@ -1,63 +1,44 @@
-var capitalLetters  = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+const generateBtn = document.querySelector("#generate");
 
-];
+const getPasswordLength = () => {
+  return 10;
+};
 
-var lowerCaseLetters = [
-  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
-];
+const getPasswordCriteria = () => {
+  return [
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "0123456789",
+    " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+  ];
+};
 
-var specialCharacters = [
-  '@','(','~','!','@','#','$','%','^','&','*','_','-','+','=','`','|','\','(',')','{','}','[',']',':',';','"',''','<','>',',','.','?','/',')',
-];
+const createRandomPassword = () => {
+  return "kdUE28(@d0";
+};
 
-var arrayOfNumbers = [0,1,2,3,4,5,6,7,8,9,10];
+// main function to generate the random password
+const generatePassword = () => {
+  // get the password length
+  const passwordLength = getPasswordLength();
 
-function passwordChoices() {
-var length = parseInt(
-  prompt ('how many characters would you like?')
+  // get the password criteria
+  const passwordCriteria = getPasswordCriteria();
 
+  // create random password
+  const password = createRandomPassword(passwordLength, passwordCriteria);
 
-);
-if (Number.isNaN(length)){
-  alert ('password length has to be numbers');
-  return null;
-}
- if (length < 8) {
-   alert('password has to be a minimum of 8 characters')
- }
+  return password;
+};
 
- if (length > 128) {
-   alert ('password length must less than 129 characters')
-   return null;
- }
+// Write password to the #password input
+const writePassword = () => {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
- varIncludeCapital = confirm ('click ok to include Capital capital letters.' );
+  passwordText.value = password;
+};
 
- varIncludeNumbers = confirm ('click ok to include numbers.');
- 
- varIncludeSpecialCharacters = confirm ('click ok to include Capital capital letters.' );
- 
- varIncludeLowerCaseLetters = confirm ('click ok to include lowercase letters.' );
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
- if (
-   IncludeCapital === false &&
-   IncludeSpecialCharacters === false &&
-   IncludeLowerCaseLetters === false &&
-   varIncludeNumbers === false &&
-   
-   )
-
-   {
-     alert ('must use atleast one character option');
-     return null;
-   }
-
-   var passwordOptions = {
-   
-    length: length,
-    IncludeCapital: IncludeCapital,
-    IncludeLowerCaseLetters: IncludeLowerCaseLetters,
-    IncludeSpecialCharacters: IncludeSpecialCharacters,
-    varIncludeNumbers: varIncludeNumbers,
-   };
